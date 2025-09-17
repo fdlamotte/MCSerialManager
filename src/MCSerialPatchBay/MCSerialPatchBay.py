0c437e105ff7b162ab222d403a3d1f2fb1ffbed2
Florent de Lamotte <florent@frizoncorrea.fr>
2025-09-17 11:48:25 +0200

    connected and disconnected re-implementation

--- src/MCSerialPatchBay/MCSerialPatchBay.py
+++ src/MCSerialPatchBay/MCSerialPatchBay.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 """ 
-    mccli.py : CLI interface to MeschCore BLE companion app
+    MCSerialPatchBay: graphical patchbay for MCSerialManager
 """
 import os, sys
 import time, datetime
 
-from QNodeEditor import Node, Edge
+from QNodeEditor import Node, Edge, Entry
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
 from QNodeEditor import NodeEditorDialog, NodeEditor
@@ -33,17 +33,53 @@ editor.scene.add_nodes([outputs, inputs])
 
 sensors = {}
 
+def edges_dict():
+    new_edges = {}
+    for s in sensors.items():
+        new_edges[s[0]] = {}
+    for e in editor.scene.edges:
+        if e.start and e.end:
+            if e.start.entry.entry_type == Entry.TYPE_OUTPUT:
+                new_edges[e.start.entry.name][e.end.entry.name]=True
+            else:
+                new_edges[e.end.entry.name][e.start.entry.name]=True
+    return new_edges
+    
 def connected():
-    edges = editor.scene.edges
-    for e in edges:
-        if e.end and e.start and not e.end.entry.name in sensors[e.start.entry.name]:
-            print(f"connect {e.start.entry.name};{e.end.entry.name}")
+    edges = edges_dict()
+    for s in edges.items():
+        for o in dict(s[1]).items():
+            if not o[0] in sensors[s[0]]:
+                print(f"connect {s[0]};{o[0]}")
+                sensors[s[0]][o[0]] = True
 
 def disconnected():
-    print("disconnecting")
-    edges = editor.scene.edges
+    edges = edges_dict()
     for s in sensors.items():
-        pass
+        for o in dict(s[1]).items():
+            if not o[0] in edges[s[0]]:
+                print(f"disconnect {s[0]};{o[0]}") 
+                del s[1][o[0]]
+
+def eval_line(line):
+    if line.startswith("sensor ") or line.startswith("s "):
+        name = line.split(" ", 1)[1]
+        if not name in outputs:
+            sensors[name] = {}
+            outputs.add_label_output(name)
+            inputs.add_label_input(name)
+            inputs[name].socket.connected.connect(connected)
+            inputs[name].socket.disconnected.connect(disconnected)
+            outputs[name].socket.connected.connect(connected)
+            outputs[name].socket.disconnected.connect(disconnected)
+
+    elif line.startswith("link ") or line.startswith("l "):
+        names = line.split(" ", 1)[1].split(";")
+        start = names[0]
+        end = names[1]
+        if not end in sensors[start]:
+            sensors[start][end]=True
+            Edge(outputs[start], inputs[end])
 
 class WorkerThread(QtCore.QThread):
     def __init__(self):
@@ -51,56 +87,21 @@ class WorkerThread(QtCore.QThread):
  
     def run(self):
         while True:
-            line = sys.stdin.readline().rstrip('\n')
-            if line.startswith("sensor "):
-                name = line.split(" ", 1)[1]
-                if not name in outputs:
-                    sensors[name] = {}
-                    outputs.add_label_output(name)
-                    inputs.add_label_input(name)
-                    inputs[name].socket.connected.connect(connected)
-                    inputs[name].socket.disconnected.connect(disconnected)
-
-            elif line.startswith("link "):
-                names = line.split(" ", 1)[1].split(";")
-                start = names[0]
-                end = names[1]
-                sensors[start][end]=True
-                Edge(outputs[start], inputs[end])
+            eval_line(sys.stdin.readline().rstrip('\n'))
 
 def main(args):
-
+    cont = True
     while True:
         line = sys.stdin.readline().rstrip('\n')
-        if line.startswith("sensor "):
-            name = line.split(" ", 1)[1]
-            if not name in outputs:
-                sensors[name] = {}
-                outputs.add_label_output(name)
-                inputs.add_label_input(name)
-                inputs[name].socket.connected.connect(connected)
-                inputs[name].socket.disconnected.connect(disconnected)
-
-        elif line.startswith("link "):
-            names = line.split(" ", 1)[1].split(";")
-            start = names[0]
-            end = names[1]
-            sensors[start][end]=True
-            Edge(outputs[start], inputs[end])
-
-        elif line == "ready":
-            break
+        if line == "r" or line == "ready":
+            break # end of init phase
+        eval_line(line)
 
     worker = WorkerThread()
     worker.start()
 
     editor.show()
     app.exec()
-
-    edges = editor.scene.edges
-
-    for e in edges:
-        print(f"{e.start.entry.name} -> {e.end.entry.name}")
 
 def cli():
     try:
